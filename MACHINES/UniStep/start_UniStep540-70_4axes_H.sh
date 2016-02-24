28c5a7dfa91b7144df19cfb0678fb2893a94544f
CoolCNC <service@thecooltool.com>
2016-02-24 17:35:36 +0100

    folder MACHINES
    auf relative Dateipfade geändert

--- MACHINES/UniStep/start_UniStep540-70_4axes_H.sh
+++ MACHINES/UniStep/start_UniStep540-70_4axes_H.sh
@@ -4,5 +4,5 @@ if [ ! "$BASH_VERSION" ] ; then
     exec /bin/bash "$0"
 fi
 cd "$( dirname "${BASH_SOURCE[0]}" )"
-source /media/coolcnc/SANDYBOX/Linux/util/init.sh
+source ../../util/init.sh
 ssh -i $KEYFILE -Y -oBatchMode=yes -oStrictHostKeyChecking=no -oUserKnownHostsFile=/dev/null machinekit@192.168.7.2 "source /etc/profile; cd ~/machinekit-configs/ && ./run.py -nm UniStep540-70_4axes_H.ini"
