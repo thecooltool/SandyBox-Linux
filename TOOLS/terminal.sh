4b64605171be86c3ab802f09971a08f1869bd7bc
Alexander Rössler <mail@roessler.systems>
2016-04-05 19:16:55 +0200

    Update terminal.sh

--- TOOLS/terminal.sh
+++ TOOLS/terminal.sh
@@ -4,5 +4,5 @@ if [ ! "$BASH_VERSION" ] ; then
     exec /bin/bash "$0"
 fi
 cd "$( dirname "${BASH_SOURCE[0]}" )"
-source util/init.sh
+source ../util/init.sh
 ssh -i $KEYFILE -Y -oStrictHostKeyChecking=no -oUserKnownHostsFile=/dev/null machinekit@192.168.7.2
